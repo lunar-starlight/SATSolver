b27d82dff93eb6249eb4cdbaa224115bdce4426c
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2020-03-24 20:20:44 +0100

    added unit clause and pure literal extraction

--- solver.cpp
+++ solver.cpp
@@ -9,6 +9,8 @@
 
 enum class clause_data : int8_t { normal, negated, unspec };
 
+typedef std::pair<int, clause_data> Literal;
+
 struct clause {
     std::vector<clause_data> term;
 
@@ -25,8 +27,9 @@ struct clause {
     }
     clause(const clause& cl) : term(cl.term) {}
 
-    std::optional<clause> unit_propagate(const int& literal, const clause_data& mode) const
+    std::optional<clause> unit_propagate(const Literal& lit) const
     {
+        auto [literal, mode] = lit;
         if (term[literal - 1] == mode) {
             return std::nullopt;
         } else if (term[literal - 1] == clause_data::unspec) {
@@ -36,6 +39,29 @@ struct clause {
             cl.term[literal - 1] = clause_data::unspec;
             return cl;
         }
+    }
+
+    std::optional<Literal> unit() const
+    {
+        int n = 0;
+        Literal lit;
+        for (size_t i = 0; i < term.size(); i++) {
+            if (term[i] != clause_data::unspec) {
+                ++n;
+                lit = std::make_pair(i + 1, term[i]);
+            }
+        }
+        if (n == 1) {
+            return lit;
+        } else {
+            return std::nullopt;
+        }
+    }
+
+    bool has_term(const Literal& lit) const
+    {
+        auto [literal, mode] = lit;
+        return term[literal - 1] == mode;
     }
 };
 
@@ -64,16 +90,51 @@ Formula parse(/*stdin*/)
     return formula;
 }
 
-Formula unit_propagate(const int& literal, const clause_data& mode, const Formula& formula)
+Formula unit_propagate(const Literal& literal, const Formula& formula)
 {
     Formula f; f.reserve(formula.size());
     for (auto&& e : formula) {
-        if (auto cl = e.unit_propagate(literal, mode)) {
+        if (auto cl = e.unit_propagate(literal)) {
             f.push_back(*cl);
         }
     }
     return f;
 }
+
+std::vector<Literal> unit_clauses(const Formula& formula)
+{
+    std::vector<Literal> units;
+    for (auto&& e : formula) {
+        if (auto p = e.unit()) {
+            units.push_back(*p);
+        }
+    }
+    return units;
+}
+
+std::vector<Literal> pure_literals(const Formula& formula)
+{
+    std::vector<Literal> pure;
+    // true if literal is a term of any of the clauses in the formula
+    auto is_present_in_formula = [ = ](Literal lit) {
+        bool b = false;
+        for (auto&& el : formula) {
+            if (!el.has_term(lit)) {
+                b = true;
+            }
+        }
+        return b;
+    };
+    for (size_t i = 0; i <  formula.front().term.size(); i++) {
+        Literal lit;
+        if (!is_present_in_formula(lit = std::make_pair(i + 1, clause_data::normal)) ||
+            !is_present_in_formula(lit = std::make_pair(i + 1, clause_data::negated))) {
+            pure.push_back(lit);  // if it is not present negated or normal, then it is pure
+        }
+    }
+    return pure;
+}
+
 
 int main()
 {
