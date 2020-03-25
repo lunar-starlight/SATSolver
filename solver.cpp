aace5a43357843b9a91d220453490d0e68c7230a
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2020-03-25 02:32:29 +0100

    created a class and implemented solution recovery

--- solver.cpp
+++ solver.cpp
@@ -14,6 +14,17 @@ enum class clause_data : int8_t { normal, negated };
 
 typedef std::pair<int, clause_data> Literal;
 
+constexpr Literal neg(const Literal& lit)
+{
+    auto [literal, mode] = lit;
+    switch (mode) {
+    case clause_data::normal:
+        return std::make_pair(literal, clause_data::negated);
+    case clause_data::negated:
+        return std::make_pair(literal, clause_data::normal);
+    }
+}
+
 struct clause {
     std::map<int, clause_data> term;
 
@@ -66,28 +77,144 @@ struct clause {
 
 };
 
-typedef std::vector<clause> Formula;
+struct Formula {
+    std::vector<clause> formula;
+    std::vector<Literal> solution;
 
-void print(const Formula& formula)
-{
-    for (auto&& cl : formula) {
-        std::cout << "(|";
-        for (auto&& [literal, mode] : cl.term) {
+    void print() const
+    {
+        for (auto&& cl : formula) {
+            std::cout << "(|";
+            for (auto&& [literal, mode] : cl.term) {
+                switch (mode) {
+                case clause_data::normal:
+                    std::cout << literal + 1 << '|';
+                    break;
+                case clause_data::negated:
+                    std::cout << '!' << literal + 1 << '|';
+                    break;
+                default:
+                    break;
+                }
+            }
+            std::cout << ')';
+        }
+        std::cout << std::endl;
+    }
+    void print_solution() const
+    {
+        for (auto&& [literal, mode] : solution) {
             switch (mode) {
             case clause_data::normal:
-                std::cout << literal + 1 << '|';
+                std::cout << literal + 1 << "=1, ";
                 break;
             case clause_data::negated:
-                std::cout << '!' << literal + 1 << '|';
-                break;
-            default:
+                std::cout << literal + 1 << "=0, ";
                 break;
             }
         }
-        std::cout << ')';
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
-}
+
+    void unit_propagate(const Literal& literal)
+    {
+        std::vector<clause> f; f.reserve(formula.size());
+        for (auto&& e : formula) {
+            if (auto cl = e.unit_propagate(literal)) {
+                f.push_back(*cl);
+            }
+        }
+        formula = f;
+    }
+
+    std::vector<Literal> unit_clauses() const
+    {
+        std::vector<Literal> units;
+        for (auto&& e : formula) {
+            if (auto p = e.unit()) {
+                units.push_back(*p);
+            }
+        }
+        return units;
+    }
+
+    std::vector<Literal> pure_literals() const
+    {
+        std::vector<Literal> pure; pure.reserve(LENGTH);
+        for (size_t i = 0; i < LENGTH; i++) {
+            Literal lit1 = std::make_pair(i, clause_data::normal);
+            Literal lit2 = std::make_pair(i, clause_data::negated);
+            bool b1 = false;
+            bool b2 = false;
+            for (auto&& el : formula) {
+                if (el.has_term(lit1)) {
+                    b1 = true;
+                }
+                if (el.has_term(lit2)) {
+                    b2 = true;
+                }
+            }
+            if (!b1 and b2) {
+                pure.push_back(lit2);
+            }
+            if (b1 and !b2) {
+                pure.push_back(lit1);
+            }
+        }
+        return pure;
+    }
+
+    bool contains_empty_clause() const
+    {
+        for (auto&& cl : formula) {
+            if (cl.term.empty()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    std::optional<std::pair<Literal, Literal>> choose_literal() const
+    {
+        for (auto&& cl : formula) {
+            if (!cl.term.empty()) {
+                return std::make_pair(*cl.term.begin(), neg(*cl.term.begin()));
+            }
+        }
+        return std::nullopt;
+    }
+
+    bool DPLL()
+    {
+        if (formula.empty()) {
+            return true;
+        }
+        if (contains_empty_clause()) {
+            return false;
+        }
+        for (auto&& el : unit_clauses()) {
+            solution.push_back(el);
+            unit_propagate(el);
+        }
+        for (auto&& el : pure_literals()) {
+            solution.push_back(el);
+            unit_propagate(el);
+        }
+        if (auto p = choose_literal()) {
+            auto [l, l_] = *p;
+            Formula ff(*this);
+            formula.push_back(clause(l, LENGTH));
+            if (DPLL()) {
+                return true;
+            } else {
+                ff.formula.push_back(clause(l_, LENGTH));
+                return ff.DPLL();
+            }
+        } else {
+            return DPLL();
+        }
+    }
+};
 
 Formula parse(/*stdin*/)
 {
@@ -106,133 +233,22 @@ Formula parse(/*stdin*/)
     LENGTH = number_of_variables;
     // std::cout << number_of_variables << ' ' << number_of_clauses << '\n';
 
-    Formula formula;
-    formula.reserve(number_of_clauses);
+    Formula f;
+    f.formula.reserve(number_of_clauses);
 
     for (int i = 0; i < number_of_clauses; ++i) {
-        formula.emplace_back(number_of_variables /*, stdin*/);
+        f.formula.emplace_back(number_of_variables /*, stdin*/);
     }
 
-    return formula;
-}
-
-Formula unit_propagate(const Literal& literal, const Formula& formula)
-{
-    Formula f; f.reserve(formula.size());
-    for (auto&& e : formula) {
-        if (auto cl = e.unit_propagate(literal)) {
-            f.push_back(*cl);
-        }
-    }
     return f;
-}
-
-std::vector<Literal> unit_clauses(const Formula& formula)
-{
-    std::vector<Literal> units;
-    for (auto&& e : formula) {
-        if (auto p = e.unit()) {
-            units.push_back(*p);
-        }
-    }
-    return units;
-}
-
-std::vector<Literal> pure_literals(const Formula& formula)
-{
-    std::vector<Literal> pure; pure.reserve(LENGTH);
-    for (size_t i = 0; i < LENGTH; i++) {
-        Literal lit1 = std::make_pair(i, clause_data::normal);
-        Literal lit2 = std::make_pair(i, clause_data::negated);
-        bool b1 = false;
-        bool b2 = false;
-        for (auto&& el : formula) {
-            if (el.has_term(lit1)) {
-                b1 = true;
-            }
-            if (el.has_term(lit2)) {
-                b2 = true;
-            }
-        }
-        if (!b1 and b2) {
-            pure.push_back(lit2);
-        }
-        if (b1 and !b2) {
-            pure.push_back(lit1);
-        }
-    }
-    return pure;
-}
-
-bool contains_empty_clause(const Formula& formula)
-{
-    for (auto&& cl : formula) {
-        if (cl.term.empty()) {
-            return true;
-        }
-    }
-    return false;
-}
-
-constexpr Literal neg(const Literal& lit)
-{
-    auto [literal, mode] = lit;
-    switch (mode) {
-    case clause_data::normal:
-        return std::make_pair(literal, clause_data::negated);
-    case clause_data::negated:
-        return std::make_pair(literal, clause_data::normal);
-    }
-}
-
-std::optional<std::pair<Literal, Literal>> choose_literal(const Formula& formula)
-{
-    for (auto&& cl : formula) {
-        if (!cl.term.empty()) {
-            return std::make_pair(*cl.term.begin(), neg(*cl.term.begin()));
-        }
-    }
-    return std::nullopt;
-}
-
-bool DPLL(Formula formula)
-{
-    if (formula.empty()) {
-        return true;
-    }
-    if (contains_empty_clause(formula)) {
-        return false;
-    }
-    for (auto&& el : unit_clauses(formula)) {
-        formula = unit_propagate(el, formula);
-    }
-    for (auto&& el : pure_literals(formula)) {
-        formula = unit_propagate(el, formula);
-    }
-    if (auto p = choose_literal(formula)) {
-        auto [l, l_] = *p;
-        formula.push_back(clause(l, LENGTH));
-        if (DPLL(formula)) {
-            return true;
-        } else {
-            formula.pop_back();
-            formula.push_back(clause(l_, LENGTH));
-            return DPLL(formula);
-        }
-    } else {
-        return DPLL(formula);
-    }
 }
 
 int main()
 {
     auto formula = parse(/*stdin*/);
 
-    // print(formula);
-
-    // for (int i = 0; i < 10; i++) {
-    std::cout << DPLL(formula) << '\n';
-    // }
+    std::cout << formula.DPLL() << '\n';
+    // formula.print_solution();
 
     return 0;
 }
